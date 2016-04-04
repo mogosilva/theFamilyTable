e4a0ccf852c73ffe62522e5652d9c492293d827b
Moises Silva <mogosilva@gmail.com>
2016-04-04 12:34:44 -0400

    Initial setup with SASS and Bootstrap

---
+++ js/main.js
@@ -0,0 +1,48 @@
+function rotateImage(image){
+	$(image).toggleClass("rotate");
+}
+
+
+
+
+
+$(document).ready(function(){
+
+
+//clicking on the ingredients arrow closes the ingredients list
+$("#ingredients img").click(function (){
+	rotateImage(this);
+	$("#ingredients ul").slideToggle();
+});
+
+//clicking on the instructions arrow closes the instructions list
+$("#instructions img").click(function (){
+	rotateImage(this);
+	$("#instructions ol").slideToggle();
+});
+
+//clicking on the Nutrition arrow closes the nutrition info
+$("#nutrition img").click(function (){
+	rotateImage(this);
+	$("#nutrition p").slideToggle();
+});
+
+//clicking on the menu causes a side menu to appear
+
+$(".menu").click(function (){
+	$(".menu").toggleClass("visible");
+	$("#offscreenMenu").toggleClass("visible");
+});
+
+$("#recipes").change(function(){
+
+	if ( $(this).val() === "cookies" ) {
+		window.location.href = "index.html";
+	}
+	else if ( $(this).val() === "brownies" ) {
+		window.location.href = "brownies.html";
+	};
+});
+
+
+});
